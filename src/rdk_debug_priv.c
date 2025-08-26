8763de4114bcee5df57d5f6ba5c9a7cd99b44acd
dshett549 <DEEPTHICHANDRASHEKAR_SHETTY@comcast.com>
2025-08-26 10:56:31 +0530

    Add rdkb_log4crc
    Signed-off-by: dshett549 <DEEPTHICHANDRASHEKAR_SHETTY@comcast.com>

--- src/rdk_debug_priv.c
+++ src/rdk_debug_priv.c
@@ -169,7 +169,6 @@ void rdk_dbg_priv_ext_init(const char* logdir, const char* log_file_name, long m
 {
     char fullpath[512];
     snprintf(fullpath, sizeof(fullpath), "%s/%s", logdir, log_file_name);
-    printf("#:Path:%s\n",fullpath);
 
     const char* cat_name = "LOG.RDK";
     log4c_category_t* cat = log4c_category_get(cat_name);
@@ -414,7 +413,6 @@ void rdk_dbg_priv_log_msg(rdk_LogLevel level, const char *module_name, const cha
 
     if(!cat)
     {
-        printf("#DEEPTHI:category not found\n");
         return;
     }
 
